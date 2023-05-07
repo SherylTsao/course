566a0f9e332903cf12d8a404ff70f163f8745d0a
LAPTOP-DQT65H7B\User <sha2672tlpu@gmail.com>
2023-05-07 15:57:30 +0800

    20230507

---
+++ src/main/java/com/example/Course_system/CourseSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.Course_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CourseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSystemApplication.class, args);
+
+	}
+
+}
